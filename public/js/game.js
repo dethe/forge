59245e05b7d5ff30559f7f0510200647a57b422f
azlen <azlen@livingcode.org>
2012-06-09 20:43:30 -0700

    added movement. working on animation

--- public/js/game.js
+++ public/js/game.js
@@ -7,13 +7,20 @@ var character = new Image();
 character.src = 'public/graphics/male_walkcycle.png';
 var characterInfo = {
 	name:"BOB",
-	speed: 5,
+	speed: 3,
 	x:30,
 	y:30,
 	sx:0,
 	sy:0,
 	w:64,
 	h:64,
+}
+
+var key = {
+	w:false,
+	a:false,
+	s:false,
+	d:false
 }
 
 function init(){
@@ -31,25 +38,58 @@ function draw(event){
 	canvas.height = window.innerHeight;
 	clear();
 	ctx.drawImage(character, characterInfo.sx, characterInfo.sy, characterInfo.w, characterInfo.h, characterInfo.x, characterInfo.y, characterInfo.w, characterInfo.h);
-	document.onkeydown = function(event) {
-  		switch (event.keyCode) {
-    		case 37: // Left
-      			characterInfo.x -= characterInfo.speed;
-    		break;
-
-    		case 38: // Up
-      			characterInfo.y -= characterInfo.speed;
-    		break;
-
-    		case 39: // Right
-      			characterInfo.x += characterInfo.speed;
-    		break;
-
-    		case 40: // Down
-      			characterInfo.y += characterInfo.speed;
-    		break;
-  		}
+	if(key.w == true){
+		characterInfo.y -= characterInfo.speed;
+	};
+	if(key.a == true){
+		characterInfo.x -= characterInfo.speed;
+	};
+	if(key.s == true){
+		characterInfo.y += characterInfo.speed;
+	};
+	if(key.d == true){
+		characterInfo.x += characterInfo.speed;
 	};
 }
+
+document.onkeydown = function(event) {
+  		switch (event.keyCode) {
+    		case 87: // Left
+      			key.w = true;
+    		break;
+
+    		case 65: // Up
+      			key.a = true;
+    		break;
+
+    		case 83: // Right
+      			key.s = true;
+    		break;
+
+    		case 68: // Down
+      			key.d = true;
+    		break;
+  		}
+};
+
+document.onkeyup = function(event) {
+  		switch (event.keyCode) {
+    		case 87: // Left
+      			key.w = false;
+    		break;
+
+    		case 65: // Up
+      			key.a = false;
+    		break;
+
+    		case 83: // Right
+      			key.s = false;
+    		break;
+
+    		case 68: // Down
+      			key.d = false;
+    		break;
+  		}
+};
 
 init();
