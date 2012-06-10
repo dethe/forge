f61c761d4422cfdf98518e9832c37c72aad70f3f
azlen <azlen@livingcode.org>
2012-06-09 19:33:05 -0700

    adding some some stuff

--- public/js/game.js
+++ public/js/game.js
@@ -1,22 +1,55 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
+var keycode;
+
+var character = new Image();
+character.src = 'public/graphics/male_walkcycle.png';
+var characterInfo = {
+	name:"BOB",
+	speed: 5,
+	x:30,
+	y:30,
+	sx:0,
+	sy:0,
+	w:64,
+	h:64,
+}
+
 function init(){
 	WIDTH = canvas.width;
 	HEIGHT = canvas.height;
-	return setInterval(draw, 1000/60)
+	return setInterval(draw, 1000/60);
 }
 
 function clear(){
 	ctx.clearRect(0, 0, WIDTH, HEIGHT);
 }
 
-function draw(){
+function draw(event){
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 	clear();
-	ctx.fillRect(10,10,10,10);
-	
+	ctx.drawImage(character, characterInfo.sx, characterInfo.sy, characterInfo.w, characterInfo.h, characterInfo.x, characterInfo.y, characterInfo.w, characterInfo.h);
+	document.onkeydown = function(event) {
+  		switch (event.keyCode) {
+    		case 37: // Left
+      			characterInfo.x -= characterInfo.speed;
+    		break;
+
+    		case 38: // Up
+      			characterInfo.y -= characterInfo.speed;
+    		break;
+
+    		case 39: // Right
+      			characterInfo.x += characterInfo.speed;
+    		break;
+
+    		case 40: // Down
+      			characterInfo.y += characterInfo.speed;
+    		break;
+  		}
+	};
 }
 
 init();
