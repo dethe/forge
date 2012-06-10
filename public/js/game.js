d54c9e51cb2e0781577379a122c06fdad0ff323b
dethe <dethe@livingcode.org>
2012-06-09 20:49:59 -0700

    added a semi-colon

--- public/js/game.js
+++ public/js/game.js
@@ -4,7 +4,7 @@ var ctx = canvas.getContext("2d");
 function init(){
 	WIDTH = canvas.width;
 	HEIGHT = canvas.height;
-	return setInterval(draw, 1000/60)
+	return setInterval(draw, 1000/60);
 }
 
 function clear(){
@@ -15,8 +15,7 @@ function draw(){
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 	clear();
-	ctx.fillRect(10,10,10,10);
-	
+	ctx.fillRect(10,10,10,10);	
 }
 
 init();
